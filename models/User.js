545943b8b693be6d8c34b4a60065deef32a0cdf1
Benjamin Liu <liubenjamin8@gmail.com>
2024-04-15 13:36:39 -0400

    MongoDB for Accounts

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true }
 }, {
-    collection: 'login'  // This specifies the MongoDB collection name
+    collection: 'codepaws.login'  // This specifies the MongoDB collection name
 });
 
 const User = mongoose.model('User', userSchema);
