69790583674fe3c7361698952fe8908defca7968
Benjamin Liu <liubenjamin8@gmail.com>
2024-04-15 13:18:40 -0400

    MongoDB for Accounts

--- server.js
+++ server.js
@@ -21,8 +21,6 @@ redisClient.connect().catch(error => {
 });
 
 mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
 }).then(() => console.log('MongoDB connected successfully.'))
     .catch(err => console.error('MongoDB connection error:', err));
 
